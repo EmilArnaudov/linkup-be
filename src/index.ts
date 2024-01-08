7cda7b4491932b4e705fad7db497c65bdc928dbf
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2024-01-08 17:34:21 +0200

    install packages

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express, { Express, Request, Response, Application } from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes';
+import 'reflect-metadata';
 
-//For env File 
+//For env File
 dotenv.config();
 
 const app: Application = express();
@@ -15,3 +16,20 @@ app.use('/', router);
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'my-secret-pw',
+//   database: 'mydb'
+// });
+
+// // Test the connection
+// connection.connect(err => {
+//   if (err) {
+//     console.error('An error occurred: ' + err.message);
+//     return;
+//   }
+
+//   console.log('Connected to the MySQL server.');
+// });
